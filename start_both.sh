7d21230e9f0e3d9f333b05b0b863a8bb6e171751
tushar bisht <tusharbisht@tushars-MacBook-Pro.local>
2025-12-29 16:23:22 +0530

    add mpdecimate back with lower threhsold to ignore frames

--- start_both.sh
+++ start_both.sh
@@ -4,6 +4,6 @@ rm frame_timestamps_ms.txt
 rm keylog.csv
 rm -y frames/*
 rm -y frames_with_keys.csv
-ffmpeg -f avfoundation -framerate 30 -use_wallclock_as_timestamps 1 -i "1:none" -filter_complex "settb=1/1000,setpts=PTS*1000,split=2[frames][ts];[frames]format=yuv420p,scale=1920:1200[out]" -map "[out]" -vsync passthrough -frame_pts 0 -q:v 2 -threads 2 frames/frame_%06d.jpg -map "[ts]" -f mkvtimestamp_v2 frame_timestamps_ms.txt &
+ffmpeg -f avfoundation -framerate 30 -use_wallclock_as_timestamps 1 -i "1:none" -filter_complex "settb=1/1000,setpts=PTS*1000,mpdecimate=hi=64*48:lo=64*24:frac=0.9,split=2[frames][ts];[frames]format=yuv420p,scale=1920:1200[out]" -map "[out]" -vsync passthrough -frame_pts 0 -q:v 2 -threads 2 frames/frame_%06d.jpg -map "[ts]" -f mkvtimestamp_v2 frame_timestamps_ms.txt &
 
 python3 keylogger/keylogger.py
